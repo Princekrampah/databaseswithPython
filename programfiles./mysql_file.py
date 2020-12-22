bddae8ce07bcf45ee7d37f255c068c1a3b6d8ce9
PrinceKrampah <jsksprince@gmail.com>
2020-12-23 01:15:27 +0300

    creating database

---
+++ programfiles./mysql_file.py
@@ -0,0 +1,184 @@
+import mysql.connector
+from mysql.connector import Error
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PASSWD = os.getenv("DATABASE_PWD")
+
+def create_connection(host_name, user_name, user_pwd):
+    connection = None
+
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd = user_pwd
+        )
+        print("Connection established")
+    except Error as e:
+        print(f"The error {e} occured")
+
+    return connection
+
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print(f"{query.split(' ')[2]} Database created.")
+    except Error as e:
+        print(f"{e} error occured")
+
+
+def create_connection_to_db(host_name, user_name, user_pwd, db_name):
+    connection = None
+
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_pwd,
+            database=db_name
+        )
+        print("Connection established")
+    except Error as e:
+        print(f"The error {e} occured")
+
+    return connection
+
+connection = create_connection("127.0.0.1", "root", PASSWD)
+
+# create_db_query = "CREATE DATABASE databasewithpython"
+# create_database(connection, create_db_query)
+
+connection = create_connection_to_db("127.0.0.1", "root", PASSWD, "databasewithpython")
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Done")
+    except Error as e:
+        print(f"{e} occured")
+
+
+create_users_table = """
+    CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT,
+        name TEXT NOT NULL,
+        age INT,
+        gender TEXT,
+        nationality TEXT,
+        PRIMARY KEY (id)
+    ) ENGINE = InnoDB;
+"""
+
+execute_query(connection, create_users_table)
+
+
+create_posts_table = """
+  CREATE TABLE IF NOT EXISTS posts(
+    id INT AUTO_INCREMENT, 
+    title TEXT NOT NULL, 
+    description TEXT NOT NULL, 
+    user_id INTEGER NOT NULL, 
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+"""
+
+
+execute_query(connection, create_posts_table)
+
+
+
+
+create_comments_table = """
+    CREATE TABLE IF NOT EXISTS comments (
+        id INT  AUTO_INCREMENT,
+        text TEXT NOT NULL,
+        user_id INT NOT NULL,
+        post_id INT NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (post_id) REFERENCES posts(id),
+        PRIMARY KEY (id)
+    )ENGINE = InnoDB;
+"""
+
+
+execute_query(connection, create_comments_table)
+
+
+create_likes_table = """
+    CREATE TABLE IF NOT EXISTS likes (
+        id INT AUTO_INCREMENT,
+        user_id INT NOT NULL,
+        post_id INT NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (post_id) REFERENCES posts(id),
+        PRIMARY KEY (id)
+    ) ENGINE = InnoDB;
+"""
+
+execute_query(connection, create_likes_table)
+
+create_users = """
+INSERT INTO
+  `users` (`name`, `age`, `gender`, `nationality`)
+VALUES
+  ('James', 25, 'male', 'USA'),
+  ('Leila', 32, 'female', 'France'),
+  ('Brigitte', 35, 'female', 'England'),
+  ('Mike', 40, 'male', 'Denmark'),
+  ('Elizabeth', 21, 'female', 'Canada');
+"""
+
+# execute_query(connection, create_users)
+
+
+create_posts = """
+INSERT INTO
+  posts (title, description, user_id)
+VALUES
+  ("Happy", "I am feeling very happy today", 1),
+  ("Hot Weather", "The weather is very hot today", 2),
+  ("Help", "I need some help with my work", 2),
+  ("Great News", "I am getting married", 1),
+  ("Interesting Game", "It was a fantastic game of tennis", 5),
+  ("Party", "Anyone up for a late-night party today?", 3);
+"""
+
+# execute_query(connection, create_posts)
+
+
+create_comments = """
+INSERT INTO
+  comments (text, user_id, post_id)
+VALUES
+  ('Count me in', 1, 6),
+  ('What sort of help?', 5, 3),
+  ('Congrats buddy', 2, 4),
+  ('I was rooting for Nadal though', 4, 5),
+  ('Help with your thesis?', 2, 3),
+  ('Many congratulations', 5, 4);
+"""
+
+create_likes = """
+INSERT INTO
+  likes (user_id, post_id)
+VALUES
+  (1, 6),
+  (2, 3),
+  (1, 5),
+  (5, 4),
+  (2, 4),
+  (4, 2),
+  (3, 6);
+"""
+
+# execute_query(connection, create_comments)
+# execute_query(connection, create_likes) 
---
+++ programfiles./sqllite_file.py
@@ -0,0 +1,152 @@
+import sqlite3
+from sqlite3 import Error
+
+
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection made to the database")
+
+    except Error as e:
+        print(f"got {e} error")
+
+    return connection
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Done")
+
+    except Error as e:
+        print(f"{e} occured")
+
+
+connection = create_connection("./mydatabase.sqlite")
+
+create_users_table = """
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        age INTEGER,
+        gender TEXT,
+        nationality TEXT
+    );
+"""
+
+# execute_query(connection, create_users_table)
+
+create_posts_table = """
+    CREATE TABLE IF NOT EXISTS posts(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        description TEXT NOT NULL,
+        user_id INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users (id)
+    );
+"""
+
+# execute_query(connection, create_posts_table)
+
+
+create_comments_table = """
+    CREATE TABLE IF NOT EXISTS comments (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        text TEXT NOT NULL,
+        user_id INTEGER NOT NULL,
+        post_id INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
+    );
+"""
+
+# execute_query(connection, create_comments_table)
+
+
+create_likes_table = """
+    CREATE TABLE IF NOT EXISTS likes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        post_id INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
+    );
+"""
+
+# execute_query(connection, create_likes_table)
+
+create_users = """
+INSERT INTO
+  users (name, age, gender, nationality)
+VALUES
+  ('James', 25, 'male', 'USA'),
+  ('Leila', 32, 'female', 'France'),
+  ('Brigitte', 35, 'female', 'England'),
+  ('Mike', 40, 'male', 'Denmark'),
+  ('Elizabeth', 21, 'female', 'Canada');
+"""
+
+# execute_query(connection, create_users)
+
+create_posts = """
+INSERT INTO
+  posts (title, description, user_id)
+VALUES
+  ("Happy", "I am feeling very happy today", 1),
+  ("Hot Weather", "The weather is very hot today", 2),
+  ("Help", "I need some help with my work", 2),
+  ("Great News", "I am getting married", 1),
+  ("Interesting Game", "It was a fantastic game of tennis", 5),
+  ("Party", "Anyone up for a late-night party today?", 3);
+"""
+
+# execute_query(connection, create_posts)
+
+create_comments = """
+INSERT INTO
+  comments (text, user_id, post_id)
+VALUES
+  ('Count me in', 1, 6),
+  ('What sort of help?', 5, 3),
+  ('Congrats buddy', 2, 4),
+  ('I was rooting for Nadal though', 4, 5),
+  ('Help with your thesis?', 2, 3),
+  ('Many congratulations', 5, 4);
+"""
+
+create_likes = """
+INSERT INTO
+  likes (user_id, post_id)
+VALUES
+  (1, 6),
+  (2, 3),
+  (1, 5),
+  (5, 4),
+  (2, 4),
+  (4, 2),
+  (3, 6);
+"""
+
+# execute_query(connection, create_comments)
+# execute_query(connection, create_likes)
+
+
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    resutls = None
+    try:
+        cursor.execute(query)
+        results = cursor.fetchall()
+    except Error as e:
+        print(e)
+    return results
+
+
+get_all_users = "SELECT * FROM users;"
+
+users = execute_read_query(connection, get_all_users)
+
+for user in users:
+    print(user)
